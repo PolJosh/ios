ff5b41930420277af4407c7be06b7e73dd0cdfe7
PolJosh <pablofranciadelbusto@gmail.com>
2019-11-29 00:08:10 -0500

    First

---
+++ Practica Swift/Models/Movie.swift
@@ -0,0 +1,10 @@
+//
+//  Movie.swift
+//  SwitfUIMovie
+//
+//  Created by PCSIJMAY (MAYTA GUILLERMO, JORGE LUIS) on 11/19/19.
+//  Copyright © 2019 UPC. All rights reserved.
+//
+
+import Foundation
+
